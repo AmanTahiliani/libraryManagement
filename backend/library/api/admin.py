e02878e04c4c84a5d487be26995b2d3f9585a43b
Aman Tahiliani <amantahiliani7437@gmail.com>
2022-10-29 18:45:56 -0400

    Added basic models and serializers

--- backend/library/api/admin.py
+++ backend/library/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from api.models import Author, Publication, Book
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Publication)
+admin.site.register(Book)
---
+++ backend/library/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+
+from django.contrib.auth.models import User
+
+from api.models import Author, Publication, Book
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = serializers.PrimaryKeyRelatedField(many =True, read_only =True)
+    class Meta:
+        model = Author
+        fields = ["name","description", "books"]
+
+class PublicationSerializer(serializers.Serializer):
+    books = serializers.PrimaryKeyRelatedField(many =True, read_only =True)
+    
+    class Meta:
+        model = Publication
+        fields = ['name', 'location', 'books']
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ['name', 'path', 'author', 'publication']
+        
