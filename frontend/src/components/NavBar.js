4e9b565aa7c45efb2e308875e2cd91136950e4c2
Roman Karmazin <rkarmazin3@gatech.edu>
2022-10-31 11:14:09 -0400

    popup window and else

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import '../components/NavBar.css'
 import Button from "@material-ui/core/Button";
-
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavBar() {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return(
         <div className="NavBar">
-            <span className="nav-logo">LOGO</span>
             <div className="nav-items">
                 <a href="/home">Home</a>
             </div>
-            <Button className="buttons" variant="contained" component="label">
+            <Button className="buttons" variant="primary" onClick={handleShow}>
                 Upload Book
-                <input hidden accept="image/*" multiple type="file" />
             </Button>
 
+            <Modal show={show} onHide={handleClose}
+                   aria-labelledby="contained-modal-title-vcenter"
+                   centered>
+                <Modal.Header closeButton>
+                    <Modal.Title >Add Book Information</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Floating className="mb-3">
+                            <Form.Control
+                                id="floatingInputCustom"
+                                type="title"
+                            />
+                            <label htmlFor="floatingInputCustom">Book Title</label>
+                        </Form.Floating>
 
+                        <Form.Floating className="mb-3">
+                            <Form.Control
+                                id="floatingInputCustom"
+                                type="author"
+                            />
+                            <label htmlFor="floatingInputCustom">Author</label>
+                        </Form.Floating>
+
+                        <Form.Floating className="mb-3">
+                            <Form.Control
+                                id="floatingInputCustom"
+                                type="publisher"
+                            />
+                            <label htmlFor="floatingInputCustom">Publisher</label>
+                        </Form.Floating>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button style={{color:"darkblue", margin: "5%", boxShadow: "5px 5px 3px rgba(46, 46, 46, 0.62)"}} variant="primary" component="label">
+                        Choose a file
+                        <input hidden accept="image/*" multiple type="file" />
+                    </Button>
+                    <Button style={{color:"darkblue", margin: "5%", boxShadow: "5px 5px 3px rgba(46, 46, 46, 0.62)"}} variant="primary" onClick={handleClose}>
+                        Save
+                    </Button>
+
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
 
 export default NavBar;
-
